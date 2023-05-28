3785d8dc8b6382f5ca9cf1955bff8dae7fc1c23f
rojanagunoori <nagunooriroja@gmail.com>
2023-05-28 11:51:22 +0530

    changed database path in post.py

--- post.py
+++ post.py
@@ -2,7 +2,7 @@ from flask import Flask, g, request, jsonify
 import sqlite3
 
 app = Flask(__name__)
-DATABASE = 'C:/Users/DELL/Desktop/crud-flask/raju.db'
+DATABASE = 'C:/Users/DELL/Desktop/crud-flask/anju.db'
 
 
 def get_db():
